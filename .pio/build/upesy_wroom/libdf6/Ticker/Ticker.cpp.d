089ccf5e8368f28ee264fb122be23d18633b646e
Phe.Chon <thiraphop.chantra@gmail.com>
2025-02-04 19:24:34 +0700

    Last

---
+++ .pio/build/upesy_wroom/libdf6/Ticker/Ticker.cpp.d
@@ -0,0 +1,6 @@
+.pio/build/upesy_wroom/libdf6/Ticker/Ticker.cpp.o: \
+ C:/Users/user/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.cpp \
+ C:/Users/user/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.h \
+ C:/Users/user/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ C:/Users/user/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ C:/Users/user/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h
